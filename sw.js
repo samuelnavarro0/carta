33ec70ca96732878cc3f8de32862b70ccbd46f63
samuelnavarro0 <105170204+samuelnavarro0@users.noreply.github.com>
2026-02-09 20:57:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-database.js');
 
-// PEGA AQUÍ TUS MISMAS CREDENCIALES
 const firebaseConfig = {
     apiKey: "AIzaSyAXhQzt9zenYwsceZBZ2D3939EykqB-HAE",
     authDomain: "cartas-amor-941ab.firebaseapp.com",
@@ -13,17 +12,19 @@ const firebaseConfig = {
     appId: "1:987328078744:web:0fa7cc713e4026cfe581ed"
 };
 
-firebase.initializeApp(firebaseConfig);
+// Inicializamos dentro del SW
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
 const db = firebase.database();
 
-// El robot se queda escuchando la base de datos de forma independiente
+// Escuchar cambios en la base de datos
 db.ref('cartas').limitToLast(1).on('child_added', (snapshot) => {
     const carta = snapshot.val();
-    
-    // Solo notificamos si la carta es muy reciente (menos de 30 segundos)
-    // para evitar que salten notificaciones viejas al reiniciar el móvil
     const ahora = Date.now();
-    if (ahora - carta.timestamp < 30000) {
+    
+    // Solo si la carta se creó hace menos de 2 minutos
+    if (ahora - carta.timestamp < 120000) {
         self.registration.showNotification("¡Nueva carta de Amor! 💌", {
             body: "Duna, tienes un mensaje: " + carta.title,
             icon: 'https://cdn-icons-png.flaticon.com/512/2904/2904973.png',
